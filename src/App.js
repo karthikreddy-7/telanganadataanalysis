e767560a82abf3dc9cba2fcf1887289b9431c2e9
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-20 20:17:30 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import LandingPage from "./landing";
 import { Analytics } from "@vercel/analytics/react";
+import { inject } from "@vercel/analytics";
 
 function App() {
   return (
     <>
       <Analytics />
+      inject();
       <LandingPage />
     </>
   );
